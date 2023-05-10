88b16ec0f4320cb2246650a9cacf35bdd32d3fa7
Gerson Ortiz <gersongio0527@gmail.com>
2023-05-09 22:55:05 -0500

    proyecto_api

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const userRouteruter = require('./user.router');
+
 const router = express.Router();
 
 // colocar las rutas aquí
-
+router.use("/user", userRouteruter);
 
 module.exports = router;
